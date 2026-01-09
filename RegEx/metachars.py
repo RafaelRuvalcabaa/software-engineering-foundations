79d30ac943ee4b84022a269ce42db2c625ce3737
RafaelRuvalcaba <157087378+RafaelRuvalcabaa@users.noreply.github.com>
2026-01-08 20:11:50 -0600

    Update more code about metachars

--- RegEx/metachars.py
+++ RegEx/metachars.py
@@ -33,3 +33,70 @@ if(found):
     print(f"Se encotró {len((found))} veces")
 else: 
     print("No se encontraron coincidencias....")
+
+
+#---------------------
+text = "casa caasa cosa cisa cessa cesa causa"
+# Con la R estamos indicando que es una expresion regular 
+pattern = r"c.sa"
+
+found = re.findall(pattern, text)
+
+if(found):
+    print(found)
+    print(f"Se encotró {len((found))} veces")
+else: 
+    print("No se encontraron coincidencias....")
+
+
+#-------------------------------
+### Con el punto busca cada carater y por lo mismo imprime cada valor separado 
+
+text = "Mi casda es blanca y mi coche negro."
+
+pattern = "."
+
+matches = re.findall(pattern, text)
+
+print(matches)
+
+#-------------------------------
+### Ahora si con esta expresión buscas los puntos. 
+### Esto se hace con diagonal invertida antes del punto.
+
+text = "Mi. cas.da es. blanca y mi coche negro."
+
+pattern = r"\."
+
+matches = re.findall(pattern, text)
+
+if(matches):
+    print(f"Encontró los siguientes valores: {matches}")
+    print(f"En total son: {len(matches)}")
+else: 
+    "No se encontró ningun valor...."
+
+
+#-------------------------------
+### Buscar digito con diagonal invertida y la letra "d", (0-9)
+
+text = "El numero de telefono es 123456789"
+
+found = re.findall(r"\d{9}", text)
+
+print(found)
+
+
+#-------------------------------
+# Ejercicio: Detectar si hay un numero de españa que es el prefijo +34 
+
+text = "Mi numero de telefono es +34 123456789 apuntalo vale?"
+
+pattern = r"\+34 \d{9}"
+
+matches = re.findall(pattern, text)
+
+if(matches):
+    print(f"Numero español encontrado: {matches}")
+else: 
+    print("No")
